94382ebc9b66629840b53c23bb971fd5adcacba5
Kevin <kevindlib@gimail.com>
2026-03-17 12:45:56 +0400

    ui: remove redundant dashboard text from sidebar header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -140,7 +140,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
             alt="Travel Lounge" 
             className="h-10 mb-3 object-contain" 
           />
-          <h1 className="text-[10px] font-black tracking-[0.2em] uppercase text-gray-500">Dashboard</h1>
+          {/* <h1 className="text-[10px] font-black tracking-[0.2em] uppercase text-gray-500">Dashboard</h1> */}
         </div>
 
         <nav className="mt-2 px-3 flex-1 overflow-y-auto custom-scrollbar pb-20">
